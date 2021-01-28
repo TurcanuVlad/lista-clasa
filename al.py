9c69f253107265bf84b4a4bd789eb8162c403aca
Turcanu Vlad <70995414+TurcanuVlad@users.noreply.github.com>
2021-01-28 10:16:57 +0300

    Add files via upload

---
+++ al.py
@@ -0,0 +1,53 @@
+orase=['didinananana','chisi..pshpshpsh','idinitsi auf','baltimor','loraxandra','travis', 'idinitsi auf', 'idinitsi auf']
+print(orase)
+#stergere el
+#del orase [2:3]
+#print(orase)
+#orase.clear()
+#print(orase)
+#orase.remove('balti')
+#print(orase)
+#orase.pop(2)
+#print(orase)
+#orase[:]=[]
+#print(orase)
+   #adaugarea el
+      #adauga 1 el
+#orase.append('sexyburger')
+#print(orase)
+   #cancatinare si alipire
+       #adauga mai multe el
+#orase.extend(['cock..<3','puzzzy'])  
+#print(orase) 
+    #inserare el
+#orase.insert(2,'ratatui')
+#print(orase)
+    #actualizare el
+#orase[2]='vavan'     
+#print(orase)
+#orase[2:4]=['dadarad']
+#print(orase)
+#orase[:]=['juicy','fingers','macdonalds','kfc','elizaveta']
+#print(orase)
+    #FELIEREA listelor
+#lista2=orase[3:6]    
+#lista1=orase[0:3]
+#print(lista1)
+#print(lista2)
+#lista1=orase[:]
+#print(orase)
+#print(orase[-2:])
+#suma=0
+#numere=[2, 3, 5, 6]
+#print('Afisare liste=', numere)
+#for element in range(0,len(numere)):
+#    suma=suma+numere[element]
+#print(suma)
+#k=orase[:]
+#for orase in k:
+#    print (orase)
+#print(orase)
+#print(orase.index('idinitsi auf'))
+#print(orase.count('idinitsi auf'))
+#print(len(orase))
+#print(max(orase))
